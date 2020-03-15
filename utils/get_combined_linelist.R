b82125e7ed11746034142e3c8f302af497a9cbaa
seabbs <s.e.abbott12@gmail.com>
2020-03-15 19:01:16 +0000

    added reference to data package

--- utils/get_combined_linelist.R
+++ utils/get_combined_linelist.R
@@ -1,12 +1,6 @@
 #' Get a combined linelist based on multiple countries data
-#'
-#' @return A linelist from multiple countries
-#' @export
-#' @importFrom tidyr drop_na
-#' @examples
-#'
 get_combined_linelist <- function() {
-  get_international_linelist("Germany") %>%
+  NCoVUtils::get_international_linelist("Germany") %>%
     dplyr::bind_rows(get_international_linelist("Italy")) %>%
     dplyr::bind_rows(get_international_linelist("France")) %>%
     dplyr::bind_rows(get_international_linelist("Spain")) %>%
